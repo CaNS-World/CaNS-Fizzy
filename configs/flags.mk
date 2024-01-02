da865c1299ddfe47311c4afaaf7168d308ae8b95
Pedro Costa <p.simoes.costa@gmail.com>
2024-01-02 14:28:02 +0100

    Making `CONSTANT_COEFFS_POISSON` mode the default.

--- configs/flags.mk
+++ configs/flags.mk
@@ -130,6 +130,10 @@ ifeq ($(strip $(MASK_DIVERGENCE_CHECK)),1)
 DEFINES += -D_MASK_DIVERGENCE_CHECK
 endif
 
+ifeq ($(strip $(CONSTANT_COEFFS_POISSON)),1)
+DEFINES += -D_CONSTANT_COEFFS_POISSON
+endif
+
 ifeq ($(strip $(OPENMP)),1)
 ifeq      ($(strip $(FCOMP)),GNU)
 override FFLAGS += -fopenmp
