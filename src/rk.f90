798056a7d0a999787e00ba14c1df72cf998ae0ff
gianlupo <gianlupo@mech.kth.se>
2024-01-24 12:07:37 +0100

    (Weighted) Youngs method based on phi (SDF)

--- src/rk.f90
+++ src/rk.f90
@@ -168,7 +168,7 @@ module mod_rk
     call swap(dsdtrk,dsdtrko)
   end subroutine rk_scal
   !
-  subroutine rk_2fl(rkpar,n,dli,dzci,dzfi,dt,gam,seps,u,v,w,psi)
+  subroutine rk_2fl(rkpar,n,dli,dzci,dzfi,dt,gam,seps,u,v,w,normx,normy,normz,psi)
     use mod_acdi     , only: acdi_transport_pf
     use mod_two_fluid, only: clip_field
     !
@@ -183,6 +183,7 @@ module mod_rk
     real(rp), intent(in   ), dimension(0:) :: dzci,dzfi
     real(rp), intent(in   ) :: gam,seps,dt
     real(rp), intent(in   ), dimension(0:,0:,0:) :: u,v,w
+    real(rp), intent(in), dimension(0:,0:,0:) :: normx,normy,normz
     real(rp), intent(inout), dimension(0:,0:,0:) :: psi
     real(rp), target     , allocatable, dimension(:,:,:), save :: dpsidtrk_t,dpsidtrko_t
     real(rp), pointer    , contiguous , dimension(:,:,:), save :: dpsidtrk  ,dpsidtrko
@@ -199,7 +200,7 @@ module mod_rk
       dpsidtrk  => dpsidtrk_t
       dpsidtrko => dpsidtrko_t
     end if
-    call acdi_transport_pf(n(1),n(2),n(3),dli(1),dli(2),dli(3),dzci,dzfi,gam,seps,u,v,w,psi,dpsidtrk)
+    call acdi_transport_pf(n,dli,dzci,dzfi,gam,seps,u,v,w,normx,normy,normz,psi,dpsidtrk)
     if(is_first) then ! use Euler forward
       !$acc kernels
       dpsidtrko(:,:,:) = dpsidtrk(:,:,:)
